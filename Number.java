923f77444b51d73e5a946c9738abb2535d77892a
MohanShan <mohankumar.scse@ksrce.ac.in>
2016-10-20 16:06:12 +0530

    Create Number.java

---
+++ Number.java
@@ -0,0 +1,21 @@
+import java.util
+class Number
+{
+public static void main(string args[])
+{
+if(a>0)
+{
+System.out.println("positive");
+}
+elseif(a<0)
+
+{
+System.out.println("negative");
+}
+else
+
+{
+System.out.println("Zero");
+}
+}
+}
