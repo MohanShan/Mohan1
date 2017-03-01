319eb926b004f28f4e81488cb66c0358f499a9ac
MohanShan <mohankumar.scse@ksrce.ac.in>
2017-03-01 02:16:46 -0800

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Hello {
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner(System.in);
+		String s1=s.next();
+		for(int i=0;i<s1.length();i++)
+		{
+			System.out.println(s1);
+		}
+		
+	}
+
+}
