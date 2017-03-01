946979cd2a318f8ff14f6c802799fb60e0fc930d
MohanShan <mohankumar.scse@ksrce.ac.in>
2017-03-01 01:40:01 -0800

    Add files via upload

---
+++ Largest.java
@@ -0,0 +1,20 @@
+
+public class Largest {
+	public static void main(String[] args)
+	{
+		int a=20,b=15,c=18;
+		if((a>b)&&(a>c))
+		{
+			System.out.println("a is big");
+		}
+		else if(b>c)
+		{
+			System.out.println("b is big");
+		}
+		else
+		{
+			System.out.println("c is big");
+		}
+	}
+
+}
