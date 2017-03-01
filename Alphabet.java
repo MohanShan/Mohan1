2ad6b34affb62533a637df0d0c3eafdbdaa291c6
MohanShan <mohankumar.scse@ksrce.ac.in>
2017-03-01 00:42:54 -0800

    Add files via upload

---
+++ Alphabet.java
@@ -0,0 +1,16 @@
+
+public class Alphabet {
+	public static void main(String[] args)
+	{
+		char a='i';
+		if(Character.isAlphabetic(a))
+		{
+			System.out.println("alphabet");
+		}
+		else
+		{
+			System.out.println("not alphabet");
+		}
+	}
+
+}
