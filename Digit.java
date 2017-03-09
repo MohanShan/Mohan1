690e97c9b3b8c9ff495315105aa02411ab3cbc1c
MohanShan <mohankumar.scse@ksrce.ac.in>
2017-03-09 15:19:33 +0530

    Add files via upload

---
+++ Digit.java
@@ -0,0 +1,13 @@
+package Mohan;
+
+import java.util.Scanner;
+
+public class Digit {
+ public static void main(String args[]){
+	 Scanner s=new Scanner(System.in);
+	 int a=s.nextInt();
+	 String str=""+a;
+	 System.out.println(str.length());
+ }
+
+}
