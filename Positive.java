7665665fdff0e096daefe2d257edf82aef26b0bb
MohanShan <mohankumar.scse@ksrce.ac.in>
2017-03-15 11:53:07 +0530

    Add files via upload

---
+++ Positive.java
@@ -0,0 +1,20 @@
+
+public class Positive {
+	public static void main(String[] args)
+	{
+		int num=0;
+		if(num>0)
+		{
+			System.out.println("positive");
+		}
+		else if(num==0)
+		{
+			System.out.println("Zero");
+		}
+		else if(num<0)
+		{
+			System.out.println("Negative");
+		}
+	}
+
+}
